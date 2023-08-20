6972d202ba6eef42a0af3c4827212a9182947455
noalhussien <noalhussien@augusta.edu>
2023-08-20 13:57:24 -0400

    add PGd attack

---
+++ exp/PGD.py
@@ -0,0 +1,11 @@
+import numpy as np
+from art.attacks.evasion import AutoProjectedGradientDescent
+
+from exp import Validatable
+
+
+class PGDConst(AutoProjectedGradientDescent, Validatable):
+
+    def generate(self, x_batch: np.ndarray, y_batch: np.ndarray = None, **kwargs) -> np.ndarray:
+        x_adv = super().generate(x_batch, y_batch)
+        return self.v_model.enforce(x_batch, x_adv)
