07173ddd48fbf27ea61498903b2eff4b3848f7ed
Neea <neearusch@gmail.com>
2023-09-20 09:43:59 -0400

    add/fix and check url constraints

--- exp/cpgd.py
+++ exp/cpgd.py
@@ -172,6 +172,60 @@ def get_lcld_constraints() -> List[BaseRelationConstraint]:
     return [g1, g2, g3, g4, g5, g6, g7, g8, g9]
 
 
+def get_url_contraints() -> List[BaseRelationConstraint]:
+    """from <https://tinyurl.com/zdypm9a8>"""
+
+    def apply_if_a_supp_zero_than_b_supp_zero(a: Feature, b: Feature):
+        return (Constant(0) <= a) or (Constant(0) < b)
+
+    g1 = Feature(1) <= Feature(0)
+
+    intermediate_sum = Constant(0)
+    for i in range(3, 18):
+        intermediate_sum = intermediate_sum + Feature(i)
+    intermediate_sum = intermediate_sum + (Constant(3) * Feature(19))
+
+    g2 = intermediate_sum <= Feature(0)
+
+    # g3: if x[:, 21] > 0 then x[:,3] > 0
+    g3 = apply_if_a_supp_zero_than_b_supp_zero(Feature(21), Feature(3))
+
+    # g4: if x[:, 23] > 0 then x[:,13] > 0
+    g4 = apply_if_a_supp_zero_than_b_supp_zero(Feature(23), Feature(13))
+
+    intermediate_sum = \
+        (Constant(3) * Feature(20) + Constant(4) * Feature(21)
+         + Constant(2) * Feature(23))
+    g5 = intermediate_sum <= Feature(0)
+
+    # g6: if x[:, 19] > 0 then x[:,25] > 0
+    g6 = apply_if_a_supp_zero_than_b_supp_zero(Feature(19), Feature(25))
+
+    # g7: if x[:, 19] > 0 then x[:,26] > 0
+    # g7 = apply_if_a_supp_zero_than_b_supp_zero(19, 26)
+
+    # g8: if x[:, 2] > 0 then x[:,25] > 0
+    g8 = apply_if_a_supp_zero_than_b_supp_zero(Feature(2), Feature(25))
+
+    # g9: if x[:, 2] > 0 then x[:,26] > 0
+    # g9 = apply_if_a_supp_zero_than_b_supp_zero(2, 26)
+
+    # g10: if x[:, 28] > 0 then x[:,25] > 0
+    g10 = apply_if_a_supp_zero_than_b_supp_zero(Feature(28), Feature(25))
+
+    # g11: if x[:, 31] > 0 then x[:,26] > 0
+    g11 = apply_if_a_supp_zero_than_b_supp_zero(Feature(31), Feature(26))
+
+    # x[:,38] <= x[:,37]
+    g12 = Feature(38) <= Feature(37)
+    g13 = (Constant(3) * Feature(20)) <= (Feature(0) + Constant(1))
+    g14 = (Constant(4) * Feature(21)) <= (Feature(0) + Constant(1))
+    g15 = (Constant(4) * Feature(2)) <= (Feature(0) + Constant(1))
+    g16 = (Constant(2) * Feature(23)) <= (Feature(0) + Constant(1))
+
+    return [g1, g2, g3, g4, g5, g6, g8, g10, g11, g12, g13, g14, g15, g16]
+
+
 def init_constraints(feat_file):
     # matches by file name - do not change.
     if 'unsw' in feat_file:
@@ -180,6 +234,8 @@ def init_constraints(feat_file):
         c_set = get_iot_constraints()
     elif 'lcld' in feat_file:
         c_set = get_lcld_constraints()
+    elif 'url' in feat_file:
+        c_set = get_url_contraints()
     else:
         c_set = None
     if not c_set or len(c_set) < 2:
