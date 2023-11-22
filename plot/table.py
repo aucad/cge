c7199e9821c10fad91348137bd4d3aa6017788f6
Neea Rusch <2580981+nkrusch@users.noreply.github.com>
2023-11-21 23:43:58 -0500

    Refactor plot (#66)

--- plot/table.py
+++ plot/table.py
@@ -13,132 +13,74 @@ class TablePlot(ResultData):
         super().__init__(directory)
         self.out = out_dir
 
-    def make_table(self, fn, mat):
-        if self.n_results == 0:
-            return
-        ensure_dir(self.out)
-        writer = SpaceAlignedTableWriter()
-        writer.headers = self.headers
-        if self.sorter:
-            mat = sorted(mat, key=self.sorter)
-        writer.value_matrix = mat
-        writer.write_table()
-        writer.dump(fn)
+    def ulist(self, fc):
+        return list(set([fc(r) for r in self.raw_rata]))
 
-    @property
-    def headers(self):
-        return ""
-
-    @property
-    def sorter(self):
-        return lambda x: (x[0], x[1], x[2])
-
-
-class EvasionPlot(TablePlot):
-
-    @property
-    def headers(self):
-        return ('classifier,exp-name,attack,accuracy,' +
-                'evades,valid,dur(s)').split(',')
-
-    @property
-    def sorter(self):
-        return lambda x: (x[0], x[1], x[2])
-
-    @staticmethod
-    def fmt(r):
-        return [
-            EvasionPlot.cls(r),
-            EvasionPlot.name(r),
-            EvasionPlot.attack(r),
-            f"{EvasionPlot.acc(r):.1f}",
-            f"{EvasionPlot.evades(r):.1f}",
-            f"{EvasionPlot.valid(r):.1f}",
-            round(r['experiment']['duration_sec'], 0)]
-
-    def write_table(self):
-        fn = self.fn_pattern('txt', 'plot', self.out)
-        mat = [EvasionPlot.fmt(r) for r in self.raw_rata]
-        self.make_table(fn, mat)
+    def make_table(self, fname, headers, mat, sorter=None):
+        if self.n_results:
+            ensure_dir(self.out)
+            fn = self.fn_pattern('txt', fname, self.out)
+            srt = sorter or (lambda x: (x[0], x[1], x[2]))
+            writer = SpaceAlignedTableWriter()
+            writer.headers = headers.split(',')
+            writer.value_matrix = sorted(mat, key=srt)
+            writer.write_table()
+            writer.dump(fn)
         return self
 
+    def evasion_plot(self):
+        def fmt(r):
+            return [
+                self.cls(r),
+                self.name(r),
+                self.attack(r),
+                f"{self.acc(r):.1f}",
+                f"{self.evades(r):.1f}",
+                f"{self.valid(r):.1f}",
+                f"{r['experiment']['duration_sec']:.0f}"]
 
-class AccuracyPlot(TablePlot):
+        hdr = 'classifier,exp-name,attack,' \
+              'accuracy,evades,valid,dur(s)'
+        mat = [fmt(r) for r in self.raw_rata]
+        return self.make_table('plot', hdr, mat)
 
-    @property
-    def headers(self):
-        return 'classifier,exp-name,accuracy ± std'.split(',')
+    def accuracy_plot(self):
+        def fmt(m, n):
+            recs = [r for r in self.raw_rata if
+                    self.cls(r) == m and self.name(r) == n]
+            a = mean([self.acc(r) for r in recs])
+            s = stdev([self.acc(r) for r in recs])
+            return m, n, f"{a:.1f} ± {s:.1f}"
 
-    @property
-    def sorter(self):
-        return lambda x: (x[0], x[1])
+        mat = [i for x in [[
+            fmt(m, n) for n in self.ulist(self.name)]
+            for m in self.ulist(self.cls)] for i in x]
+        hdr = 'classifier,exp-name,accuracy±σ'
+        return self.make_table('cls_acc', hdr, mat)
 
-    def calc_acc(self, model, name):
-        recs = [r for r in self.raw_rata if
-                AccuracyPlot.cls(r) == model and
-                AccuracyPlot.name(r) == name]
-        avg = mean([AccuracyPlot.acc(r) for r in recs])
-        std = stdev([AccuracyPlot.acc(r) for r in recs])
-        return avg, std
+    def config_plot(self):
+        def fmt(r):
+            e, v = r['experiment'], r['validation']
+            i, p = set(v['immutable']), v['predicates']
+            d = set([i for x in v['dependencies'].values() for i in x])
+            a = set(list(r['experiment']['attrs'])[:-1])
+            f = (sum([len(m['attrs']) for m in p.values()]) /
+                 max(1, len(p)))
+            return tuple([
+                self.name(r), e['n_records'], e['n_classes'],
+                e['n_attributes'] - 1,  # exclude class label
+                len(a - i - d),  # feat that can take on any value
+                v['n_constraints'], len(i), len(p), f"{f:.1f}"])
 
-    @staticmethod
-    def num_fmt(a, b, sep='±'):
-        return f"{a:.1f} {sep} {b:.1f}"
-
-    def write_table(self):
-        fn, mat = self.fn_pattern('txt', 'cls_acc', self.out), []
-        models = [AccuracyPlot.cls(r) for r in self.raw_rata]
-        names = [AccuracyPlot.name(r) for r in self.raw_rata]
-        for m in list(set(models)):
-            for n in list(set(names)):
-                acc = self.calc_acc(m, n)
-                mat.append([m, n, self.num_fmt(*acc)])
-        self.make_table(fn, mat)
-
-
-class ConfigPlot(TablePlot):
-
-    @property
-    def headers(self):
-        return 'exp-name,records,cls,ft,*-val,#-C,' \
-               '⊥,P,ft/P'.split(',')
-
-    @property
-    def sorter(self):
-        return lambda x: (x[0])
-
-    @staticmethod
-    def fmt(r):
-        ex = r['experiment']
-        attrs = list(ex['attrs'])[:-1]
-        res = [
-            ConfigPlot.name(r),
-            ex['n_records'],
-            ex['n_classes'],
-            len(attrs)]
-        if v := r['validation']:
-            I = v['immutable']
-            P = v['predicates']
-            V = [len(m['attrs']) for m in P.values()]
-            D = [i for x in v['dependencies'].values() for i in x]
-            res.append(len(list(set(attrs) - set(I) - set(D))))
-            res.append(v['n_constraints'])
-            res.append(len(I))
-            res.append(len(P))
-            res.append(f"{sum(V) / len(P):.1f}") if len(P) else None
-        return res
-
-    def write_table(self):
-        fn = self.fn_pattern('txt', 'config', self.out)
-        mat = [tuple(ConfigPlot.fmt(r)) for r in self.raw_rata]
-        self.make_table(fn, list(set(mat)))
-        return self
+        mat = self.ulist(fmt)
+        hdr = 'exp-name,records,cls,ft,*val,#C,⊥,P,ft/P'
+        return self.make_table('config', hdr, mat)
 
 
 def plot_results(directory, out=None):
     print('Results for directory:', directory)
-    ConfigPlot(directory, (out or directory)).write_table()
-    AccuracyPlot(directory, (out or directory)).write_table()
-    res = EvasionPlot(directory, (out or directory)).write_table()
-    if res.n_results > 0:
-        res.show_duration()
+    tp = TablePlot(directory, (out or directory)) \
+        .config_plot().accuracy_plot().evasion_plot()
+
+    if tp.n_results > 0:
+        tp.show_duration()
